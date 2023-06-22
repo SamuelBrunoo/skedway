ee9bcdcb383280a94b1e59284d05f69d02acc789
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-06-22 10:11:49 -0300

    removed /face-wizard redirect, and alerting public url

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,12 @@
-import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom"
+import { RouteObject, createBrowserRouter } from "react-router-dom"
 
 import RegisterPage from "./pages/Register"
 import ErrorPage from "./pages/ErrorPage"
 
 const routes: RouteObject[] = [
   {
-    path: '/face-wizard',
-    element: <RegisterPage />
-  },
-  {
     path: '/',
-    element: <Navigate to={'/face-wizard'} />
+    element: <RegisterPage />
   },
   {
     path: '*',
