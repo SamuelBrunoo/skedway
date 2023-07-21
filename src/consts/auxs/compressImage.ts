32eabbd5ca1078bd733d9f47cb899385675fe755
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-07-21 15:13:22 -0300

    mobile prev img crop using FileReader

---
+++ src/consts/auxs/compressImage.ts
@@ -0,0 +1,33 @@
+export const compressImage = (blob: Blob): Promise<{ url: string; blob: Blob; }> => {
+
+  return new Promise((resolve, reject) => {
+    const finalW = 640
+
+    const reader = new FileReader
+    reader.readAsDataURL(blob)
+
+    reader.onload = e => {
+      let imgUrl = e.target?.result
+      let image = document.createElement("img")
+      image.src = imgUrl as string
+
+      image.onload = async (l: any) => {
+        let canvas = document.createElement("canvas")
+        const ratio = finalW / l.target.width
+
+        canvas.width = finalW
+        canvas.height = l.target.height * ratio
+
+        const context = canvas.getContext("2d")
+        context?.drawImage(image, 0, 0, canvas.width, canvas.height)
+
+        const newImgUrl = context?.canvas.toDataURL("image/webp", 90) as string
+        let newImgBlob = await fetch(newImgUrl).then(r => r.blob())
+
+        document.body.innerHTML = `<img src="${newImgUrl}" style="max-width:100%;" />`
+
+        // resolve({ url: newImgUrl, blob: newImgBlob })
+      }
+    }
+  })
+}
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -15,6 +15,7 @@ import ComponentSelect from "../../components/ComponentSelect";
 import FaceAutoCapture from "../../components/FaceAutoCapture";
 import SuccessSubmit from "../../components/SuccessSubmit";
 import renderLogo from "../../consts/auxs/renderLogo";
+import { compressImage } from "../../consts/auxs/compressImage";
 
 function RegisterPage() {
   const [pageError, setPageError] = useState<'token' | 'generic' | null>(null)
@@ -39,6 +40,8 @@ function RegisterPage() {
     const imageUrl = URL.createObjectURL(image)
     setPhotoUrl(imageUrl)
     setPhotoBlob(image)
+
+    setTimeout(async () => await compressImage(image), 800)
   };
 
   const handleFaceCapturePhotoTaken = (image: Blob, data?: any) => {
@@ -108,7 +111,7 @@ function RegisterPage() {
     }
   };
 
-  useEffect(() => {
+  useEffect(() => {/*
     if (!token) {
       setPageError("token")
       return
@@ -127,16 +130,16 @@ function RegisterPage() {
             setLoading(false)
           }
         })
-    }
+    }*/
   }, [token])
 
-  return (pageError) ? (
+  return (/*pageError) ? (
     <ErrorPage error={(pageError === "token") ? 'accessDenied' : 'generic'} />
   ) : (loading) ? (
     <div className={styles.loadingContainer}>
       <LoadingComponent />
     </div>
-  ) : (
+  ) : (*/
     <div className={styles.app}>
       <header className={styles.header}>
         {userInfo && renderLogo(userInfo.company.logo)}
