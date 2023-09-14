91d7148d5bc74143261a99adb5cf93cce6c1a9eb
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-09-14 11:28:36 -0300

    set first request to videoframe

--- src/components/_steps/Capture/index.tsx
+++ src/components/_steps/Capture/index.tsx
@@ -15,8 +15,9 @@ const Capture = () => {
   }
 
   const renderCapture = async () => {
-    const sdkToken = await Api.getOnfidoSDKToken()
-    const worflowRunId = await Api.getWorkflowRunId()
+    const sdkToken = await Api.getVideoFrame('e1e551d6-3e6a-4144-9ef9-2a1dc9190490')
+    // const sdkToken = await Api.getOnfidoSDKToken()
+    // const worflowRunId = await Api.getWorkflowRunId()
 
     // @ts-ignore
     loadUI(sdkToken, worflowRunId, completeFn)
