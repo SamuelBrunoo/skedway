f2b1b68317083b6134245bb9ddf2bfe1a3b37c2f
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-10-05 14:44:59 -0300

    Implemented dinamic language getter

--- src/_lang/index.ts
+++ src/_lang/index.ts
@@ -1,5 +1,5 @@
-import portuguese from './portuguese.ts'
-import english from './english.ts'
+import portuguese from "./portuguese.ts"
+import english from "./english.ts"
 import spanish from "./spanish.ts"
 
 const langs = {
@@ -8,6 +8,16 @@ const langs = {
   es: spanish,
 }
 
-const currentLanguage: 'pt' | 'en' | 'es' = 'pt'
+const getLang = () => {
+  const navLanguage = window.navigator.language
+
+  if (/en/.test(navLanguage)) return "en"
+  else if (/pt/.test(navLanguage)) return "pt"
+  else if (/es/.test(navLanguage)) return "es"
+
+  return "en"
+}
+
+const currentLanguage: "pt" | "en" | "es" = getLang()
 
 export default langs[currentLanguage]
